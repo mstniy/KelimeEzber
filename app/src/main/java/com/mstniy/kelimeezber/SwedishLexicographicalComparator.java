d1b0558a275f0928ec6af26ee367cc436195ff4f
Kartal Kaan Bozdoğan <kartalkaanbozdogan@gmail.com>
2019-02-10 01:19:22 +0300

    SLC now considers accented characters equal to their unaccented counterparts

--- app/src/main/java/com/mstniy/kelimeezber/SwedishLexicographicalComparator.java
+++ app/src/main/java/com/mstniy/kelimeezber/SwedishLexicographicalComparator.java
@@ -6,36 +6,36 @@ public class SwedishLexicographicalComparator {
     public static boolean compare(String a, String b) { // < comparsion
         HashMap<Character, Integer> charMap = new HashMap<>();
         charMap.put('a',  0);
-        charMap.put('ä',  1);
-        charMap.put('å',  2);
-        charMap.put('b',  3);
-        charMap.put('c',  4);
-        charMap.put('d',  5);
-        charMap.put('e',  6);
-        charMap.put('f',  7);
-        charMap.put('g',  8);
-        charMap.put('h',  9);
-        charMap.put('i', 10);
-        charMap.put('j', 11);
-        charMap.put('k', 12);
-        charMap.put('l', 13);
-        charMap.put('m', 14);
-        charMap.put('n', 15);
-        charMap.put('o', 16);
-        charMap.put('ö', 17);
-        charMap.put('p', 18);
-        charMap.put('q', 19);
-        charMap.put('r', 20);
-        charMap.put('s', 21);
-        charMap.put('t', 22);
-        charMap.put('u', 23);
-        charMap.put('v', 24);
-        charMap.put('w', 25);
-        charMap.put('x', 26);
-        charMap.put('y', 27);
-        charMap.put('z', 28);
-        charMap.put('-', 29);
-        charMap.put('.', 30);
+        charMap.put('ä',  0);
+        charMap.put('å',  0);
+        charMap.put('b',  1);
+        charMap.put('c',  2);
+        charMap.put('d',  3);
+        charMap.put('e',  4);
+        charMap.put('f',  5);
+        charMap.put('g',  6);
+        charMap.put('h',  7);
+        charMap.put('i',  8);
+        charMap.put('j',  9);
+        charMap.put('k', 10);
+        charMap.put('l', 11);
+        charMap.put('m', 12);
+        charMap.put('n', 13);
+        charMap.put('o', 14);
+        charMap.put('ö', 14);
+        charMap.put('p', 15);
+        charMap.put('q', 16);
+        charMap.put('r', 17);
+        charMap.put('s', 18);
+        charMap.put('t', 19);
+        charMap.put('u', 20);
+        charMap.put('v', 21);
+        charMap.put('w', 22);
+        charMap.put('x', 23);
+        charMap.put('y', 24);
+        charMap.put('z', 25);
+        charMap.put('-', 26);
+        charMap.put('.', 27);
 
         int minLength = a.length(); // Awkward. API level 23 doesn't have integer min?
         if (b.length() < minLength)
@@ -44,9 +44,9 @@ public class SwedishLexicographicalComparator {
             Integer codeA = charMap.get(Character.toLowerCase(a.charAt(i)));
             Integer codeB = charMap.get(Character.toLowerCase(b.charAt(i)));
             if (codeA == null)
-                codeA = 31;
+                codeA = charMap.size();
             if (codeB == null)
-                codeB = 31;
+                codeB = charMap.size();
             if (codeA != codeB)
                 return codeA < codeB;
         }
