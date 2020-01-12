e249c4b9dba9c009372479ba0ea928ec15c1e028
Kartal Kaan Bozdoğan <kartalkaanbozdogan@gmail.com>
2020-01-12 16:33:31 +0300

    Listening exercises are now multiple-choice, where the user select the spoken sentence amoung three random others.

--- app/src/main/java/com/mstniy/kelimeezber/ListeningFragment.java
+++ app/src/main/java/com/mstniy/kelimeezber/ListeningFragment.java
@@ -1,32 +1,20 @@
 package com.mstniy.kelimeezber;
 
 import android.content.Context;
-import android.net.Uri;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
-import com.google.android.flexbox.FlexboxLayout;
-import com.opencsv.CSVReader;
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Random;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 class AudioAndWords implements Serializable {
     String audioPath;
@@ -45,9 +33,9 @@ public class ListeningFragment extends Fragment {
 
     MyApplication app;
     boolean created = false;
-    Button hintButton, replayButton;
-    TextView hintView;
-    FlexboxLayout wordTableInput, wordTableOptions;
+    Button replayButton;
+    Button buttons[] = new Button[4];
+    boolean buttonsHighlighted[] = new boolean[4];
     AudioAndWords p = null;
 
     public void onAttach(Context context) {
@@ -59,13 +47,6 @@ public class ListeningFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) throws RuntimeException{
         app = (MyApplication) getContext().getApplicationContext();
         View rootView = inflater.inflate(R.layout.fragment_listening, container, false);
-        hintView = rootView.findViewById(R.id.hint_view);
-        hintButton = rootView.findViewById(R.id.hint_button);
-        hintButton.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                HintButtonClicked();
-            }
-        });
         replayButton = rootView.findViewById(R.id.replay_button);
         replayButton.setOnClickListener(new OnClickListener() {
             @Override
@@ -73,8 +54,17 @@ public class ListeningFragment extends Fragment {
                 ReplayButtonClicked();
             }
         });
-        wordTableInput = rootView.findViewById(R.id.word_box_input);
-        wordTableOptions = rootView.findViewById(R.id.word_box_options);
+        buttons[0] = rootView.findViewById(R.id.option1);
+        buttons[1] = rootView.findViewById(R.id.option2);
+        buttons[2] = rootView.findViewById(R.id.option3);
+        buttons[3] = rootView.findViewById(R.id.option4);
+        for (int i=0; i<4; i++)
+            buttons[i].setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ButtonClicked((Button)v);
+                }
+            });
         created = true;
         SoftKeyboardHelper.hideSoftKeyboard(getActivity());
         return rootView;
@@ -84,69 +74,39 @@ public class ListeningFragment extends Fragment {
         newRound(app.ats.get(new Random().nextInt(app.ats.size())));
     }
 
-    TextView CreateButton(String text) {
-        TextView b = new TextView(getContext());
-        b.setText(text);
-        b.setBackgroundResource(android.R.drawable.btn_default);
-        b.setTextSize(20.0f);
-        b.setGravity(Gravity.CENTER);
-        b.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                ButtonClicked((TextView) v);
-            }
-        });
-        return b;
-    }
-
     void newRound(AudioAndWords _p) {
         if (created == false)
             return ;
+        for (int i = 0; i < 4; i++)
+            ChangeColorOfButton(i, false);
         p = _p;
-        hintView.setText("");
-        wordTableInput.removeAllViews();
-        wordTableOptions.removeAllViews();
         if (p == null)
             return ;
-        ArrayList<String> shuffledWords = new ArrayList<>();
-        shuffledWords.addAll(p.words);
-        Collections.shuffle(shuffledWords);
-        for (String word : shuffledWords)
-            wordTableOptions.addView(CreateButton(word));
+        for (int i=0; i<4; i++)
+            buttons[i].setText(app.ats.get(new Random().nextInt(app.ats.size())).sentence);
+        int correctAnswer = new Random().nextInt(4);
+        buttons[correctAnswer].setText(p.sentence);
         app.playAudio(p.audioPath);
     }
 
-    void ButtonClicked(TextView button) {
-        if (wordTableInput.indexOfChild(button) == -1) {
-            button.setEnabled(false);
-            wordTableInput.addView(CreateButton(button.getText().toString()));
-
-            if (wordTableInput.getChildCount() == p.words.size()) {
-                boolean correctAnswer = true;
-                for (int i=0; i<p.words.size(); i++)
-                    if (((TextView)wordTableInput.getChildAt(i)).getText().equals(p.words.get(i)) == false) {
-                        correctAnswer = false;
-                        break;
-                    }
-                if (correctAnswer)
-                    newRound();
-            }
-
-        } else {
-            wordTableInput.removeView(button);
-            final String buttonText = button.getText().toString();
-            for (int i=0; i<wordTableOptions.getChildCount(); i++) {
-                TextView child = (TextView) wordTableOptions.getChildAt(i);
-                if (child.getText().toString().equals(buttonText) && child.isEnabled() == false) {
-                    child.setEnabled(true);
-                    break;
-                }
-            }
-            // Note that a round can only end after the user clicks on one of the word options, thus adding it to the input box.
-        }
+    private void ChangeColorOfButton(int buttonIndex, boolean highlight) {
+        Button button = buttons[buttonIndex];
+        if (highlight)
+            button.setBackgroundColor(Color.rgb(100, 255, 100));
+        else
+            button.setBackgroundResource(android.R.drawable.btn_default);
+        buttonsHighlighted[buttonIndex] = highlight;
     }
 
-    void HintButtonClicked() {
-        hintView.setText(p.sentence);
+    void ButtonClicked(Button button) {
+        if (button.getText().equals(p.sentence)) {
+            newRound();
+        }
+        else {
+            for (int i=0; i<4; i++) // Highlight the correct answer
+                if (buttons[i].getText().equals(p.sentence))
+                    ChangeColorOfButton(i, true);
+        }
     }
 
     @Override
@@ -155,22 +115,12 @@ public class ListeningFragment extends Fragment {
 
         outState.putSerializable("p", p);
 
-        outState.putBoolean("hintVisible", hintView.getText().length() != 0);
+        CharSequence[] buttonTexts = new CharSequence[4];
+        for (int i=0;i<4;i++)
+            buttonTexts[i] = buttons[i].getText();
+        outState.putCharSequenceArray("buttonTexts", buttonTexts);
 
-        String[] inputWords = new String[wordTableInput.getChildCount()];
-        for (int i=0; i<wordTableInput.getChildCount(); i++)
-            inputWords[i] = ((TextView)wordTableInput.getChildAt(i)).getText().toString();
-        outState.putCharSequenceArray("inputWords", inputWords);
-
-        String[] optionWords = new String[wordTableOptions.getChildCount()];
-        boolean[] optionsEnabled = new boolean[wordTableOptions.getChildCount()];
-        for (int i=0; i<wordTableOptions.getChildCount(); i++) {
-            optionWords[i] = ((TextView) wordTableOptions.getChildAt(i)).getText().toString();
-            optionsEnabled[i] = wordTableOptions.getChildAt(i).isEnabled();
-        }
-        outState.putCharSequenceArray("optionWords", optionWords);
-
-        outState.putBooleanArray("optionsEnabled", optionsEnabled);
+        outState.putBooleanArray("buttonsHighlighted", buttonsHighlighted);
     }
 
     @Override
@@ -183,22 +133,13 @@ public class ListeningFragment extends Fragment {
 
         p = (AudioAndWords) savedInstanceState.getSerializable("p");
 
-        if (savedInstanceState.getBoolean("hintVisible"))
-            HintButtonClicked();
-        else
-            hintView.setText("");
+        CharSequence[] buttonTexts = savedInstanceState.getCharSequenceArray("buttonTexts");
+        for (int i=0; i<4; i++)
+            buttons[i].setText(buttonTexts[i]);
 
-        CharSequence[] inputWords = savedInstanceState.getCharSequenceArray("inputWords");
-        for (CharSequence inputWord : inputWords)
-            wordTableInput.addView(CreateButton(inputWord.toString()));
-
-        CharSequence[] optionWords = savedInstanceState.getCharSequenceArray("optionWords");
-        boolean[] optionsEnabled = savedInstanceState.getBooleanArray("optionsEnabled");
-        for (int i=0; i<optionWords.length; i++) {
-            TextView button = CreateButton(optionWords[i].toString());
-            button.setEnabled(optionsEnabled[i]);
-            wordTableOptions.addView(button);
-        }
+        boolean[] buttonsHighlighted = savedInstanceState.getBooleanArray("buttonsHighlighted");
+        for (int i=0; i<4; i++)
+            ChangeColorOfButton(i, buttonsHighlighted[i]);
     }
 
     @Override
